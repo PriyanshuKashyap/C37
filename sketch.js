c6c9f8743b267995601b1a07b02c7c72f96e4d70
PriyanshuKashyap <74148663+PriyanshuKashyap@users.noreply.github.com>
2021-04-03 13:15:15 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var database;
+var canvas;
+var gameState = 0;
+var playerCount;
+var form, player, game;
+var allPlayers;
+var car1, car2, car3, car4;
+var cars;
+function setup(){
+    database = firebase.database();
+    console.log(database);
+    canvas = createCanvas(displayWidth - 200, displayHeight - 150);
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+   if (playerCount === 4) {
+       game.update(1);
+   }
+   if (gameState === 1) {
+       clear();
+       game.play();
+   }
+   
+}
